a8244c84445e1913d065b9348e5750b203783455
IMarunasheva <i.marunasheva@mail.ru>
2025-01-03 17:14:27 +0300

    Добавьте файлы проекта.

---
+++ Module_12.1.5/Program.cs
@@ -0,0 +1,66 @@
+﻿using System;
+using System.Xml.Linq;
+using static System.Runtime.InteropServices.JavaScript.JSType;
+
+namespace Module_12._1._5
+{
+    class Program
+    {
+        private static bool isPremium;
+        private static readonly List<User> users = [];
+
+        static void Main(string[] args)
+        {
+            while (true)
+            {
+                try
+                {
+                    Console.Write("Введите логин: ");
+                    string login = Console.ReadLine();
+
+                    Console.Write("Введите имя: ");
+                    string name = Console.ReadLine();
+
+                    Console.Write("У вас есть премиум-подписки да/нет: ");
+                    string answerPremium = Console.ReadLine();
+
+                    if (string.IsNullOrEmpty(login) || string.IsNullOrEmpty(name) || string.IsNullOrEmpty(answerPremium))
+                    {
+                        throw new ArgumentNullException("Значения логин/пароль/наличие подписки не могут быть пустыми");
+                    }
+
+                    if (answerPremium != "Да" && answerPremium != "да" && answerPremium != "Нет" && answerPremium != "нет")
+                    {
+                        throw new FormatException("Укажите наличие подписки (Да или Нет)");
+                    }
+
+                    if (answerPremium == "Да" || answerPremium == "да")
+                    {
+                        isPremium = true;
+                    }
+                    else
+                    {
+                        isPremium = false;
+                    }
+
+                    User user = new(login, name, isPremium);
+                    users.Add(user);
+                    user.Greetings();
+                    user.Advertisements();
+
+                    Console.WriteLine();
+                }
+                catch (ArgumentNullException ex)
+                {
+                    Console.WriteLine(ex.Message);
+                    Console.WriteLine();
+                }
+                catch (FormatException ex)
+                {
+                    Console.WriteLine(ex.Message);
+                    Console.WriteLine();
+                }
+            }
+        }
+    }
+}
---
+++ Module_12.1.5/User.cs
@@ -0,0 +1,54 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Module_12._1._5
+{
+    public class User
+    {
+        public string Login { get; set; }
+        public string Name { get; set; }
+        public bool IsPremium { get; set; }
+
+        public User(string login, string name, bool isPremium) 
+        {
+            Login = login;
+            Name = name;
+            IsPremium = isPremium;
+        }
+        public void Greetings()
+        {
+            Console.WriteLine("Здравствуйте, {0}", Name);
+        }
+
+        static void ShowAds()
+        {
+            Console.WriteLine("Посетите наш новый сайт с бесплатными играми free.games.for.a.fool.com");
+            // Остановка на 1 с
+            Thread.Sleep(1000);
+
+            Console.WriteLine("Купите подписку на МыКомбо и слушайте музыку везде и всегда.");
+            // Остановка на 2 с
+            Thread.Sleep(2000);
+
+            Console.WriteLine("Оформите премиум-подписку на наш сервис, чтобы не видеть рекламу.");
+            // Остановка на 3 с
+            Thread.Sleep(3000);
+        }
+
+        public void Advertisements()
+        {
+            if (IsPremium == false)
+            {
+                ShowAds();
+            }
+            else
+            {
+                Console.WriteLine();
+                Console.WriteLine("* * * * * * * * *");
+            }
+        }
+    }
+}
